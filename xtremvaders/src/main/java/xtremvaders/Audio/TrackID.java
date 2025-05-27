efdefce3ef7a11087cffba0c16bb96b45fc123bd
dgolay <102981354+Lancoli@users.noreply.github.com>
2025-05-28 00:47:40 +0200

    mapped 4 news songs. pre-added more

--- xtremvaders/src/main/java/xtremvaders/Audio/TrackID.java
+++ xtremvaders/src/main/java/xtremvaders/Audio/TrackID.java
@@ -1,11 +1,10 @@
 package xtremvaders.Audio;
 
 public enum TrackID {
-    // Genre DnB
     SPACE_QUEST(new AudioTrack("space_quest", SoundType.MUSIC, 1, 175)),
-    TECIDORS(new AudioTrack("tecidors", SoundType.MUSIC, 1, 174)),
-
-    // Techno
+    VESTA(new AudioTrack("Gemi_Kori_Vesta", SoundType.MUSIC, 1, 174)),
+    CAREFUL(new AudioTrack("Thorjn-Careful", SoundType.MUSIC, 1, 174)),
+    PORCELAIN(new AudioTrack("Ruckspin_Porcelain", SoundType.MUSIC, 1, 174)),
     LOST_LANDS(new AudioTrack("lostLands", SoundType.MUSIC, 1, 130));
 
     private final AudioTrack track;
